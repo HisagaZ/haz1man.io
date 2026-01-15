dca921c7dd276d949d19151a164c5e44377d0fe5
HisagaZ <82130717+HisagaZ@users.noreply.github.com>
2026-01-15 18:10:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
+const artworkItems = document.querySelectorAll(".artwork-item");
+
+artworkItems.forEach(item => {
+    const img = item.querySelector("img");
+
+    item.addEventListener("mousemove", (e) => {
+        const rect = item.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const moveX = ((x / rect.width) - 0.5) * 20;
+        const moveY = ((y / rect.height) - 0.5) * 20;
+
+        img.style.transform = `translate(${moveX}px, ${moveY}px) scale(1.15)`;
+    });
+
+    item.addEventListener("mouseleave", () => {
+        img.style.transform = "translate(0, 0) scale(1)";
+    });
+});
+
+
